0a6e2bf29d49962d07d2d35b6db1f0788bd68be1
Subhasis Pal <palsubhasis100@gmail.com>
2024-10-26 20:04:07 +0530

    Created Homepage

---
+++ client/src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">My Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+        
+      </header>
+
+   
+          <div className="post">
+            <div className="image">
+              <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg"></img>
+            </div>
+            <div className="texts">
+              <h2>Post Heading</h2>
+              <p className="info">
+                <a className="author"> Subhro</a>
+                <time>2024-10-26 7:44</time>
+              </p>
+              <p className="summary">Download and use 700000+ Beautiful stock photos for free. ✓ Thousands of new images every day ✓ Completely Free to Use ✓</p>
+            </div>
+            
+          </div>
+          <div className="post">
+            <div className="image">
+              <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg"></img>
+            </div>
+            <div className="texts">
+              <h2>Post Heading</h2>
+              <p className="info">
+                <a className="author"> Subhro</a>
+                <time>2024-10-26 7:44</time>
+              </p>
+              <p className="summary">Download and use 700000+ Beautiful stock photos for free. ✓ Thousands of new images every day ✓ Completely Free to Use ✓</p>
+            </div>
+            
+          </div>
+          <div className="post">
+            <div className="image"> 
+              <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg"></img>
+            </div>
+            <div className="texts">
+              <h2>Post Heading</h2>
+              <p className="info">
+                <a className="author"> Subhro</a>
+                <time>2024-10-26 7:44</time>
+              </p>
+              <p className="summary">Download and use 700000+ Beautiful stock photos for free. ✓ Thousands of new images every day ✓ Completely Free to Use ✓</p>
+            </div>
+            
+          </div>
+    </main>
+  );
+}
+
+export default App;
